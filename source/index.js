c81c7a3097bfa774deaf6069a357863b8febee7b
Kyle Mitchell <kyle@kemitchell.com>
2015-04-29 21:24:58 +0000

    refactor "transforms"

--- source/index.js
+++ source/index.js
@@ -2,6 +2,32 @@ var spdx = require('spdx');
 
 var valid = spdx.valid.bind(spdx);
 
+var transforms = [
+  function(argument) {
+    return argument.toUpperCase();
+  },
+  function(argument) {
+    return argument.trim();
+  },
+  function(argument) {
+    return argument.replace(/\s+/g, '');
+  },
+  function(argument) {
+    return argument.replace(/\s*(\d)/, '-$1');
+  },
+  function(argument) {
+    return argument.replace(/\s*(\d)/, '-$1');
+  },
+  function(argument) {
+    return argument.replace(/\s*V\s*(\d)/, '-$1');
+  },
+  function(argument) {
+    return argument
+      .replace(/\s*V\s*(\d)/, '-$1')
+      .replace(/(\d)$/, '$1.0');
+  }
+];
+
 var lastResorts = [
   ['APACHE', 'Apache-2.0'],
   ['AFFERO', 'AGPL-3.0'],
@@ -18,35 +44,26 @@ var lastResorts = [
   ['Unli', 'Unlicense']
 ];
 
+var SUBSTRING = 0;
+var IDENTIFER = 1;
+
 module.exports = function(identifier) {
+  var i;
+
+  for (i = 0; i < transforms.length; i++) {
+    var transformed = transforms[i](identifier);
+    if (valid(transformed)) {
+      return transformed;
+    }
+  }
+
   var upperCased = identifier.toUpperCase();
-  if (valid(upperCased)) {
-    return upperCased;
-  }
-  if (valid(identifier.trim())) {
-    return identifier.trim();
-  }
-  if (/\s*(\d)/.test(identifier)) {
-    var hyphenated = identifier.replace(/\s*(\d)/, '-$1');
-    if (valid(hyphenated)) {
-      return hyphenated;
+  for (i = 0; i < lastResorts.length; i++) {
+    var lastResort = lastResorts[i];
+    if (upperCased.indexOf(lastResort[SUBSTRING]) > -1) {
+      return lastResort[IDENTIFER];
     }
   }
-  if (/\s*V\s*\d/.test(identifier)) {
-    var withHyphen = identifier.replace(/\s*V\s*(\d)/, '-$1');
-    if (valid(withHyphen)) {
-      return withHyphen;
-    }
-    var dotZero = withHyphen.replace(/(\d)$/, '$1.0');
-    if (valid(dotZero)) {
-      return dotZero;
-    }
-  }
-  for (var i = 0; i < lastResorts.length; i++) {
-    var pair = lastResorts[i];
-    if (upperCased.indexOf(pair[0]) > -1) {
-      return pair[1];
-    }
-  }
+
   return null;
 };
